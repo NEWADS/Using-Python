58407fd0629f6290cc3cc0f225f32d0258705160
Wilson Zhang <sidghj@gmail.com>
2018-11-19 22:06:41 +0800

    add baseline correction function

--- ECGFilters.py
+++ ECGFilters.py
@@ -1,4 +1,9 @@
 import scipy.io as sio
+import numpy as np
+from scipy import sparse
+from scipy.sparse.linalg import spsolve
+import matplotlib.pyplot as plt
+
 
 """
 Author: Wilson.Zhang
@@ -10,7 +15,29 @@ The ULg Multimodality Drowsiness Database (called DROZY) and Examples of Use.
 
 # Load ECG signals
 ECG = sio.loadmat("ECG1_1.mat")["ECG1_1"]
-print(ECG, ECG.shape)
+# print(ECG, ECG.shape)
 
-# Set filters
 
+# Set Baseline Correction Filter...
+# Reference: https://stackoverflow.com/questions/29156532/python-baseline-correction-library
+def baseline_als(y, lam, p, niter=10):
+    L = len(y)
+    D = sparse.diags([1, -2, 1], [0, -1, -2], shape=(L, L-2))
+    w = np.ones(L)
+    for i in range(niter):
+        W = sparse.spdiags(w, 0, L, L)
+        Z = W + lam * D.dot(D.transpose())
+        z = spsolve(Z, w*y)
+        w = p * (y > z) + (1-p) * (y < z)
+    return z
+
+
+tmp = ECG[0]
+res = baseline_als(tmp, 0.1, 0.1)
+print(res, res.shape)
+
+# Try to plot res out...
+time = range(len(res))
+plt.figure()
+plt.plot(time, -res)
+plt.show()
